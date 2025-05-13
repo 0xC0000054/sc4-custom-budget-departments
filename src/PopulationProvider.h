342b932623fb578ebe5c27e5308e94883663715c
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-05-13 07:19:37 -0600

    Update copyright year and re-license to LGPL 3.0
    Changing to LGPL 3.0 ensures that modified version of the plugin must
    include source code if they are distributed.

--- src/PopulationProvider.h
+++ src/PopulationProvider.h
@@ -1,15 +1,24 @@
-////////////////////////////////////////////////////////////////////////
-//
-// This file is part of sc4-custom-budget-departments, a DLL Plugin for
-// SimCity 4 that allows new budget departments to be added to the game
-// using a building's exemplar.
-//
-// Copyright (c) 2024 Nicholas Hayes
-//
-// This file is licensed under terms of the MIT License.
-// See LICENSE.txt for more information.
-//
-////////////////////////////////////////////////////////////////////////
+/*
+ * This file is part of sc4-custom-budget-departments, a DLL Plugin for
+ * SimCity 4 that allows new budget departments to be added to the game
+ * using a building's exemplar.
+ *
+ * Copyright (C) 2024, 2025 Nicholas Hayes
+ *
+ * sc4-custom-budget-departments is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * sc4-custom-budget-departments is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with sc4-custom-budget-departments.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 #include "IPopulationProvider.h"
